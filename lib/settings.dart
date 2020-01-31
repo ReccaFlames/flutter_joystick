5b025329d79bf95d917200b1aaa4dd49cc3a32f5
Pawel Jozwik X <pawel.jozwik2@sabre.com>
2020-02-01 00:04:26 +0100

    Added Settings Page content

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_joystick/commons/portrait_stateful_mixin.dart';
+
+import 'commons/landscape_stateful_mixin.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
@@ -10,13 +11,19 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsPageState extends State<SettingsPage>
-    with PortraitStatefulModeMixin<SettingsPage> {
+    with LandscapeStatefulModeMixin<SettingsPage> {
+  bool _devMode = false;
+  String _url = 'https://jsonplaceholder.typicode.com/posts';
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings', style: TextStyle(color: Colors.grey[700]),),
+        title: Text(
+          'Settings',
+          style: TextStyle(color: Colors.grey[700]),
+        ),
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -24,7 +31,41 @@ class SettingsPageState extends State<SettingsPage>
           color: Colors.grey[700], //change your color here
         ),
       ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.color_lens),
+            title: Text('Colors'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              print('navigate');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.code),
+            title: Text('Endpoint'),
+            trailing: Text('$_url'),
+          ),
+          ListTile(
+            leading: Icon(Icons.developer_mode),
+            title: Text('Developer mode'),
+            trailing: Switch(
+              value: _devMode,
+              onChanged: (value) => setDevMode(value),
+            ),
+            onTap: () {
+              setDevMode(!_devMode);
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  void setDevMode(bool value) {
+    setState(() {
+      _devMode = value;
+    });
   }
 
   @override
@@ -36,5 +77,4 @@ class SettingsPageState extends State<SettingsPage>
   void initState() {
     super.initState();
   }
-
 }
