fc9aac351eb793a84812483145015172f1bf99ac
Pawel Jozwik X <pawel.jozwik2@sabre.com>
2020-02-02 22:00:54 +0100

    Added change endpoint url functionality

--- lib/commons/shared_preferences_helper.dart
+++ lib/commons/shared_preferences_helper.dart
@@ -4,6 +4,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesHelper {
   static final String _backgroundColor = "backgroundColor";
   static final String _joystickColor = "joystickColor";
+  static final String _endpointUrl = "endpointUrl";
+
+  static Future<String> getEndpointUrl() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String endpointUrl = prefs.getString(_endpointUrl);
+    return endpointUrl != null && endpointUrl != '' ? endpointUrl : 'https://jsonplaceholder.typicode.com/posts';
+  }
+
+  static Future<bool> setEndpointUrl(String endpointUrl) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setString(_endpointUrl, endpointUrl);
+  }
 
   static Future<Color> getBackgroundColor() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
